b70e010e350318812f41e6b21ac91740159a5c35
bkopanichuk <43197664+bkopanichuk@users.noreply.github.com>
2019-05-16 23:05:32 +0300

    Перезалив файлов в другой бренч

---
+++ lab2/Progression.h
@@ -0,0 +1,75 @@
+//
+//  Progression.h
+//  laba9
+//
+//  Created by Богдан on 5/11/19.
+//  Copyright © 2019 Guryanova Dasha. All rights reserved.
+//
+
+#ifndef Progression_h
+#define Progression_h
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+vector<int> generate_AProgression(int first_member = 1, int step = 1, int progression_lenght = 10, int max_member = 99999999){//Генерация арефметической прогрессии по заданным параметрам
+    int a = first_member;
+    vector<int> result;
+    result.push_back(a);
+    for (int i = 1; i < progression_lenght && abs(a) <= max_member-abs(step); i++){
+        a = a + step;
+        result.push_back(a);
+    }
+    return result;
+}
+
+vector<int> generate_GProgression(int first_member = 1, int step = 2, int progression_lenght = 10, int max_member = 99999999){//Генерация геометрической прогрессии по заданным параметрам
+    int a = first_member;
+    vector<int> result;
+    result.push_back(a);
+    for (int i = 1; i < progression_lenght && abs(a) <= max_member/abs(step); i++){
+        a = a * step;
+        result.push_back(a);
+    }
+    return result;
+}
+
+int summ_AProgression(int first_member = 1, int step = 1, int progression_lenght = 10){//Сумма арефметической прогрессии по заданным параметрам
+    return ((2*first_member + step*(progression_lenght-1))*progression_lenght)/2;
+}
+
+int summ_GProgression(int first_member = 1, int step = 2, int progression_lenght = 10){//Сумма геометрической прогрессии по заданным параметрам
+        return (first_member*(1-pow(step, progression_lenght)))/(1-step);
+}
+
+bool is_AProgression(vector<int> tmp){//Проверка вектора, есть ли арефметической прогрессией
+    int a = tmp[1]-tmp[0];
+    for (int i = 1; i < tmp.size()-1; i++){
+        if (tmp[i+1]-tmp[i] != a){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool is_GProgression(vector<int> tmp){//Проверка вектора, есть ли геометрической прогрессией
+    int a = tmp[1]/tmp[0];
+    for (int i = 1; i < tmp.size()-1; i++){
+        if (tmp[i+1]/tmp[i] != a){
+            return false;
+        }
+    }
+    return true;
+}
+
+int member_AProgression(int first_member = 1, int step = 1, int N_of_member = 10){//нахождение члена арефметической прогрессии по заданным параметрам
+    return first_member + step*(N_of_member - 1);
+}
+
+int member_GProgression(int first_member = 1, int step = 1, int N_of_member = 10){//нахождение члена геометрической прогрессии по заданным параметрам
+    return first_member*pow(step,(N_of_member - 1));
+}
+
+
+#endif /* Progression_h */
---
+++ lab2/pifagorthree.h
@@ -0,0 +1,88 @@
+//
+//  pifagorthree.h
+//  laba9
+//
+//  Created by Богдан on 5/11/19.
+//  Copyright © 2019 Guryanova Dasha. All rights reserved.
+//
+
+#ifndef pifagorthree_h
+#define pifagorthree_h
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+bool is_pifagorthree(int x, int y, int z){ //проверка, является ли тройка пифагоровой
+    if (x*x + y*y == z*z){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int NOD(int a, int b) { // Наибольший Общий Делитель
+    if (a == b) {
+        return a;
+    }
+    if (a > b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    return NOD(a, b - a);
+}
+
+bool is_primitive_pifagorthree(int x, int y, int z){ //Проверка, является ли пифагорова тройка примитивной
+    if (is_pifagorthree(x, y, z) && NOD(x, y) == 1 && NOD(x, z) == 1 && NOD(y, z) == 1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+vector<vector<int>> generate_pifagorthrees(int upper_limit = 100, int lower_limit = 1){ //Нахождение всех пифагоровых троек по диапазону (), по стандарту (1:100), можно задавать только верхнюю границу, можно обе
+    vector<vector<int>> result;
+    for (int x = lower_limit; x < upper_limit; x++){
+        for (int y = lower_limit; y < upper_limit; y++){
+            for (int z = lower_limit; z < upper_limit; z++){
+                if (is_pifagorthree(x, y, z)){
+                    vector<int> tmp;
+                    tmp.push_back(x);
+                    tmp.push_back(y);
+                    tmp.push_back(z);
+                    result.push_back(tmp);
+                }
+            }
+        }
+    }
+    return result; //Возвращает вектор с векторами из 3-х значений (x, y, z)
+}
+
+vector<vector<int>> generate_primitive_pifagorthrees(int upper_limit = 100, int lower_limit = 1){ //Нахождение всех простых пифагоровых троек по диапазону, аналогично предыдущему
+    vector<vector<int>> result;
+    for (int m = lower_limit; m < sqrt(upper_limit); m++){
+        for (int n = lower_limit; n < sqrt(upper_limit); n++){
+            int x = m*m - n*n;
+            int y = 2*m*n;
+            int z = m*m + n*n;
+            if (z <= upper_limit && x>0 && y>0 && z> 0 && NOD(x, y) == 1 && NOD(x, z) == 1 && NOD(y, z) == 1){
+                vector<int> tmp;
+                tmp.push_back(x);
+                tmp.push_back(y);
+                tmp.push_back(z);
+                result.push_back(tmp);
+            }
+        }
+    }
+    return result; //Возвращает вектор с векторами из 3-х значений (x, y, z)
+}
+
+void print_result(vector<vector<int>> tmp){ //Вывод результата в консоль
+    for (auto i = tmp.begin(); i != tmp.end(); i++){
+        cout << i->data()[0] << " " << i->data()[1] << " " << i->data()[2] << endl;
+    }
+}
+
+
+#endif /* pifagorthree_h */
